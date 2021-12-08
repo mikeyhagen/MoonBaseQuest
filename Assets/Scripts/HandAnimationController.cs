1cdbf1b5f74a13b5fd57f1265ae24d8d7ea53d59
mikeyhagen <mikeyhagen@gmail.com>
2021-12-07 22:06:05 -0600

    Added hand script as well as hand Animations

--- Assets/Scripts/HandAnimationController.cs
+++ Assets/Scripts/HandAnimationController.cs
@@ -10,19 +10,54 @@ public class HandAnimationController : MonoBehaviour
     public InputDeviceCharacteristics controllerType;
     public InputDevice thisController;
 
+    private bool isControllerDetected = false;
+    private Animator animatorController;
     // Start is called before the first frame update
     void Start()
     {
+        Initialise();
+        animatorController = GetComponent<Animator>();
+
+    }
+
+
+    void Initialise()
+    {
         List<InputDevice> controllerDevices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(controllerType, controllerDevices);
-
-        thisController = controllerDevices[0];
-        Debug.Log(thisController.name);
+        if (controllerDevices.Count.Equals(0))
+        {
+            Debug.Log("List is empty");
+        }
+        else
+        {
+            thisController = controllerDevices[0];
+            //Debug.Log(thisController.name);
+            isControllerDetected = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!isControllerDetected)
+        {
+            Initialise();
+        }
+        else
+        {
+            if (thisController.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+            {
+                Debug.Log("Trigger Press");
+                animatorController.SetFloat("Trigger", triggerValue);
+            }
+            if (thisController.TryGetFeatureValue(CommonUsages.grip, out float gripValue) && gripValue > 0.1f)
+            {
+                Debug.Log("Grip Press");
+                animatorController.SetFloat("Grip", gripValue);
+            }
+        }
         
+
     }
 }
