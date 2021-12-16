4c3ccdead516c63ca6aa00d16226e26814e1d0cc
mikeyhagen <mikeyhagen@gmail.com>
2021-12-15 22:57:40 -0600

    Added laser fire

--- Assets/Scripts/FireLaserGun.cs
+++ Assets/Scripts/FireLaserGun.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class FireLaserGun : MonoBehaviour
 {
+    public Animator gunAnimator;
+    public GameObject laserBeamModel;
+    public Transform laserSpawnPoint;
+    public Transform laserParent;
     public void FireGun()
     {
+        //Access the animator on the gun model, trigger the fire animation
+        gunAnimator.SetTrigger("Fire");
+
+        //when the trigger is pressed, play the audio.
+        GetComponent<AudioSource>().Play();
+
+        //instantiate the laser beam
+        GameObject generatedLaserBeam = Instantiate(laserBeamModel,laserSpawnPoint.position,laserSpawnPoint.rotation);
+
+        //put new laser into parent
+        generatedLaserBeam.transform.SetParent(laserParent);
+
         Debug.Log("Laser fire");
     }
 }
---
+++ Assets/Scripts/LaserBeamMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamMove : MonoBehaviour
+{
+    public float thrust;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb.velocity = transform.forward * thrust;
+
+        Destroy(gameObject, 2f);
+    }
+}
